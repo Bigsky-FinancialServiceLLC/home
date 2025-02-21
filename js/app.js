7fbb93c85a7d30c1708ed52d792016ea6b122516
PhobiaCide <aamason86@gmail.com>
2025-02-20 21:27:50 -0600

    update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-/*
-=======
-
->>>>>>> 325770e (initial build)
 const fadeSections = document.querySelectorAll('.section-fade');
 const options = { threshold: 0.2 };
 const revealOnScroll = new IntersectionObserver((entries, observer) => {
@@ -15,18 +10,3 @@ const revealOnScroll = new IntersectionObserver((entries, observer) => {
 }, options);
 
 fadeSections.forEach(sec => revealOnScroll.observe(sec));
-<<<<<<< HEAD
-*/
-
-// Toggle mobile menu visibility
-function toggleMobileMenu() {
-  var menu = document.getElementById('mobile-menu');
-  menu.classList.toggle('d-none');
-}
-
-
-document
-  .getElementById('menu-toggle')
-  .addEventListener('click', toggleMobileMenu);
-=======
->>>>>>> 325770e (initial build)
