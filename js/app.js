a0d75343f8e565626c7493779190475e3b96f4cc
PhobiaCide <aamason86@gmail.com>
2025-02-23 12:08:35 -0600

    update

--- js/app.js
+++ js/app.js
@@ -13,12 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     modifyStylesheetHref(mode) {
       const isMobile = window.matchMedia("(max-width: 768px)").matches;
-
       const topcoatVersion = isMobile ? `mobile-${mode}` : `desktop-${mode}`;
-
-   
         this.topcoatStylesheet.href = `https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/css/topcoat-${topcoatVersion}.min.css`;
-      
     }
     static reverse(mode) {
       return mode === "dark" ? "light" : "dark";
@@ -106,4 +102,66 @@ document.addEventListener("DOMContentLoaded", function () {
   /* Icomatic Initialization */
   IcomaticUtils.run();
   new ColorMode();
+
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 8, density: { enable: true, value_area: 800 } },
+      color: { value: "#6699cc" },
+      shape: {
+        type: "circle",
+        stroke: { width: 1, color: "#000000" },
+        polygon: { nb_sides: 1 },
+        image: { src: "img/github.svg", width: 100, height: 100 }
+      },
+      opacity: {
+        value: 1,
+        random: false,
+        anim: { enable: false, speed: 1, opacity_min: 1, sync: false }
+      },
+      size: {
+        value: 0,
+        random: false,
+        anim: { enable: false, speed: 4, size_min: 0, sync: false }
+      },
+      line_linked: {
+        enable: true,
+        distance: 1500,
+        color: "#6699cc",
+        opacity: 1,
+        width: 7
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: true,
+        straight: true,
+        out_mode: "out",
+        bounce: true,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "push" },
+        resize: true
+      },
+      modes: {
+        grab: { distance: 400, line_linked: { opacity: 1 } },
+        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+        repulse: { distance: 200, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 }
+      }
+    },
+    retina_detect: true
+  });
+  var update;
+  update = function () {
+    requestAnimationFrame(update);
+  };
+  requestAnimationFrame(update);
+
 });
